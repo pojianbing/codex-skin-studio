18b50f4000608ddc8c2f89925721833411c3dc69
pojianbing <76243339@qq.com>
2026-08-14 16:57:35 +0800

    release: v0.4.3

--- src/components/app-updater.tsx
+++ src/components/app-updater.tsx
@@ -18,7 +18,7 @@ import { cn } from '@/lib/utils'
 
 type UpdateState = 'idle' | 'checking' | 'available' | 'downloading' | 'ready'
 
-const fallbackVersion = '0.4.2'
+const fallbackVersion = '0.4.3'
 
 function formatUpdateDate(value?: string) {
   if (!value) return '刚刚发布'
